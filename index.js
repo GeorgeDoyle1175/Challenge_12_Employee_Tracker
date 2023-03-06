214295c808f78633ba01bad0e7af5bfd4bb8aade
Patrick <doyle1175@gmail.com>
2023-03-06 18:32:44 -0500

    Set up Connection

---
+++ index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'company_db'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database.');
+});
